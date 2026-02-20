ff17edd285a1dc7d9c01858c03388e9dd83204a2
henny215 <zackhennigan215@gmail.com>
2026-02-20 17:01:03 -0500

    Update app.py

--- app.py
+++ app.py
@@ -287,9 +287,9 @@ SNOWFALL_HTML = """
     <div class="bar-row">
       <div class="bar-label-row">
         <span class="bar-label">1 &ndash; 3 in</span>
-        <span class="bar-percent">10%</span>
+        <span class="bar-percent">8%</span>
       </div>
-      <div class="bar-track"><div class="bar-fill" style="width:10%;"></div></div>
+      <div class="bar-track"><div class="bar-fill" style="width:8%;"></div></div>
     </div>
     <div class="bar-row">
       <div class="bar-label-row">
